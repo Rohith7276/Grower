db4cb3f784ff29ed290720b471af4c86194b0a02
Rohith7276 <140833328+Rohith7276@users.noreply.github.com>
2024-06-22 20:11:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// Smooth scroll to target section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+  
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+  
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
